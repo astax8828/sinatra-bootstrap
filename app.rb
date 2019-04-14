3920bee6396f3ac941e0919a50098f89a6450f87
Stas Chekhovskikh <astax8828@gmail.com>
2019-04-14 18:17:49 +1100

    Added gem 'email_validator'

--- app.rb
+++ app.rb
@@ -3,6 +3,7 @@ require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
 require 'sinatra/activerecord'
+require 'email_validator'
 
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
